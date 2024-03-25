d018bef8dbf41b69a26ff12bd20fafaeaa644715
MrYeeOus <benwmyee@gmail.com>
2024-03-25 12:05:19 +1100

    Convert curly brackets to square for JSONL format

--- data/daeToJson.py
+++ data/daeToJson.py
@@ -17,14 +17,19 @@ for root, dirs, files in os.walk(directory):
                 dt = re.sub(r"[\n\s]+", "", dt)
                 descript = {"GENERATED_DESCRIPTION" : dt}
                 descript = str(descript).replace("'", "\"")
+                descript = descript.replace("{", "[")
+                descript = descript.replace("}", "]")
 
             with open("export_data/" + filename) as fs:
 # Convert the .dae data
                 doc = xmltodict.parse(fs.read())
                 data = {"GENERATED_DATA" : doc}
                 data = str(data).replace("'", "\"")
+                data = data.replace("{", "[")
+                data = data.replace("}", "]")
             with open("./export_data2/" + fn + ".json", "w") as outfile:
                 towrite = str([descript, data])
+                towrite = "{" + towrite[1:len(towrite)-1] + "}"
                 outfile.write(towrite.replace("'", ''))
 
 """
